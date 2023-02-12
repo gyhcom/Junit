cdb374e2e2ffa94a60d9b00e7fa716943e71fbf6
gyhcom <gyeonho@gamil.com>
2023-02-12 21:29:34 +0900

    commit test2

--- src/test/java/com/junit/domain/BookRepositoryTest.java
+++ src/test/java/com/junit/domain/BookRepositoryTest.java
@@ -11,7 +11,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 @DataJpaTest
 class BookRepositoryTest {
 
-    @Autowired //DI1
+    @Autowired //DI2
     private BookRepository bookRepository;
     //1. 책 등록
     @Test
