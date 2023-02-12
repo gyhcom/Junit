23c91f7e7a1dd0ded1135f4f9ac2313ce668eb8b
gyhcom <gyeonho@gamil.com>
2023-02-12 19:29:42 +0900

    BookSRequest create

--- src/test/java/com/junit/domain/BookRepositoryTest.java
+++ src/test/java/com/junit/domain/BookRepositoryTest.java
@@ -2,6 +2,7 @@ package com.junit.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.junit.domain.entity.Book;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +17,23 @@ class BookRepositoryTest {
     @Test
     @DisplayName("책 등록 테스트")
     void save_test(){
-        System.out.println("책 등록");
+        //given (데이터 준비)
+        
+        String title="junit5";
+        String author="Gyh";
+
+        Book book = Book.builder()
+            .title(title)
+            .author(author)
+            .build();
+        
+        //when  (테스트 실행)
+        Book bookPs = bookRepository.save(book);
+
+        assertEquals(title, bookPs.getTitle());
+        assertEquals(author, bookPs.getAuthor());
+
+        //then  (검증)
     }
     //2. 책 목록보기
     //3. 책 한건보기
